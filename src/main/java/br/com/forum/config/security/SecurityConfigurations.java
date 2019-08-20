392847406cdbd471f4fb1ec39c03e4f5f193bc40
andregodasi <andre.goncalves@wises.com.br>
2019-08-20 19:00:28 -0300

    inicio da implementacao de autenticacao

---
+++ src/main/java/br/com/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,37 @@
+package br.com.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	//Configuracoes de autenticacao
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
+	
+	//Configuracoes de Autorizacao
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated();
+		
+		
+	}
+	
+	//Configuracoes de recursos estaticos(js, css. imagens, etc)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
+}
